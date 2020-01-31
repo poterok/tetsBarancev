d7bf4b8e6a976ecb4f95441f858397788a3c3181
Dimov <poterok@gmil.com>
2020-01-31 18:40:48 +0300

    add application

--- application.py
+++ application.py
@@ -1,43 +1,41 @@
+
 from selenium import webdriver
-import time
 
 
-
-
-
-
-class Aplication:
+class Application:
 
     def __init__(self):
         self.driver = webdriver.Chrome()
-        self.vars = {}
+        self.driver.implicitly_wait(60)
 
     def open_site(self):
         self.driver.get("https://yandex.ru")
 
-    def login(self, group):
+    def login(self, group_auth):
         self.driver.find_element_by_link_text('Войти в почту').click()
         time.sleep(1)
-        self.driver.find_element_by_name('login').send_keys(group.username)
+        self.driver.find_element_by_name('login').send_keys(group_auth.username)
         time.sleep(1)
         self.driver.find_element_by_class_name('passp-flex-wrapper')
         time.sleep(1)
         self.driver.find_element_by_class_name('button2_type_submit').click()
         time.sleep(1)
-        self.driver.find_element_by_id('passp-field-passwd').send_keys(group.password)
+        self.driver.find_element_by_id('passp-field-passwd').send_keys(group_auth.password)
         time.sleep(1)
         # Подтвердить
         self.driver.find_element_by_class_name('button2_type_submit').click()
         time.sleep(3)
 
+
     def logout(self):
-        time.sleep(4)
+        time.sleep(1)
         self.driver.find_element_by_css_selector('div.mail-User-Name').click()
+        time.sleep(1)
+        self.driver.find_element_by_css_selector('div.ui-dialog.ui-widget.ui-widget-content.ui-corner-all.ui-front._nb-popup-outer.ui-dialog-no-close._nb-popup_to_bottom').find_element_by_link_text('Выйти из сервисов Яндекса').click()
         time.sleep(2)
-        self.driver.find_element_by_css_selector(
-            'div.ui-dialog.ui-widget.ui-widget-content.ui-corner-all.ui-front._nb-popup-outer.ui-dialog-no-close._nb-popup_to_bottom').find_element_by_link_text(
-            'Выйти из сервисов Яндекса').click()
-        time.sleep(2)
+
+
+
 
     def go_to_contacts(self):
         self.driver.find_element_by_link_text('Контакты').click()
@@ -45,23 +43,20 @@ class Aplication:
 
     def create_new_group(self, group_name):
         # Создать группу
-        self.driver.find_element_by_class_name('mail-LabelList').find_element_by_class_name(
-            '_nb-button-content').click()
+        self.driver.find_element_by_class_name('mail-LabelList').find_element_by_class_name('_nb-button-content').click()
         time.sleep(1)
 
         # Ввести название новой группы
-        self.driver.find_element_by_class_name('ui-dialog').find_element_by_class_name(
-            'mail-AbookPopup-Right').find_element_by_class_name('nb-input').send_keys(group_name)
+        self.driver.find_element_by_class_name('ui-dialog').find_element_by_class_name('mail-AbookPopup-Right').find_element_by_class_name('nb-input').send_keys(group_name)
         time.sleep(1)
 
         # Сохранить название группы
-        self.driver.find_element_by_class_name('ui-dialog').find_element_by_class_name(
-            'mail-AbookPopup-Selected-Buttons').find_element_by_css_selector('button.nb-button').click()
+        self.driver.find_element_by_class_name('ui-dialog').find_element_by_class_name('mail-AbookPopup-Selected-Buttons').find_element_by_css_selector('button.nb-button').click()
         time.sleep(1)
 
-    def go_to_new_group(self, group_name):
+    def go_to_new_group(self):
         # Перейти в группу
-        self.driver.find_element_by_link_text(group_name).click()
+        self.driver.find_element_by_link_text('Новая группа').click()
         time.sleep(2)
 
     def go_to_settings_group(self):
@@ -71,8 +66,7 @@ class Aplication:
 
     def choose_group_for_delete(self):
         # # Выбрать группу для удаления
-        self.driver.find_element_by_xpath(
-            '//*[@id="nb-1"]/body/div[2]/div[6]/div/div[3]/div[3]/div[2]/div[5]/div/div/div[2]/div[2]/ul/li[3]/span[2]').click()
+        self.driver.find_element_by_xpath('//*[@id="nb-1"]/body/div[2]/div[6]/div/div[3]/div[3]/div[2]/div[5]/div/div/div[2]/div[2]/ul/li[3]/span[2]').click()
         time.sleep(2)
 
     def delete_group(self):
@@ -80,7 +74,5 @@ class Aplication:
         self.driver.find_element_by_xpath('//*[@id="nb-12"]/span/span/span').click()
         time.sleep(3)
 
-
-
-    def destroy_app(self):
+    def destrot(self):
         self.driver.quit()
