0b9ce28a08f904dc9294c64343164662ed3c2e16
Dimov <poterok@gmil.com>
2020-01-31 18:25:15 +0300

    Add Group Add method Group in tests

--- group_auth.py
+++ group_auth.py
@@ -1,8 +1,6 @@
-<<<<<<< HEAD:group_auth.py
 
 class Group:
     def __init__(self, username, password):
         self.username = username
         self.password = password
-=======
->>>>>>> Revert "Add group PageObject":group.py
+
