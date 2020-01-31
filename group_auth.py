350eb458ba97ecd5c8571aafe982d034a8a989f1
Dimov <poterok@gmil.com>
2020-01-31 17:55:43 +0300

    Revert "Add group PageObject"
    This reverts commit 7be7337c

--- group_auth.py
+++ group_auth.py
@@ -1,5 +1,8 @@
+<<<<<<< HEAD:group_auth.py
 
 class Group:
     def __init__(self, username, password):
         self.username = username
         self.password = password
+=======
+>>>>>>> Revert "Add group PageObject":group.py
