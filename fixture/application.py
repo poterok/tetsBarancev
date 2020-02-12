f989c4b48ac86bb636e1dfacc0eb465bfa25c3e2
Dimov <poterok@gmil.com>
2020-02-12 18:35:22 +0300

    Нихуйя не робит

--- fixture/application.py
+++ fixture/application.py
@@ -2,6 +2,7 @@
 from selenium import webdriver
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
+
 import time
 
 
--- fixture/group.py
+++ fixture/group.py
@@ -1,4 +1,7 @@
 import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class GroupHelper:
 
@@ -45,28 +48,32 @@ class GroupHelper:
 
 
 
-
-
         # Если уже есть группа и нужно создать еще
 
     def if_has_other_group(self, group_name):
-        if self.app.driver.find_element_by_class_name('mail-LabelList').find_element_by_class_name(
-                '_nb-button-content').click() is not None:
-            time.sleep(2)
-            self.app.driver.find_element_by_link_text(group_name.name).click()
-            time.sleep(2)
-            self.app.driver.find_element_by_class_name('mail-LabelList-Setup').find_element_by_link_text(
-                'Создать группу').click()
+
+        # Кнопка "Создать группу "
+        try:
+            # a = self.app.driver.find_element_by_xpath('//*[@id="nb-1"]/body/div[2]/div[6]/div/div[3]/div[2]/div[3]/div/div[1]/div/div/div[3]/div/div[2]/button/span/span')
+            s = WebDriverWait(self.app.driver, 1).until(EC.presence_of_element_located((By.XPATH, '//*[@id="nb-1"]/body/div[2]/div[6]/div/div[3]/div[2]/div[3]/div/div[1]/div/div/div[3]/div/div[2]/button/span/span')))
+        except:
+            s = None
+        print(s)
+        if s is None:
+            # self.app.driver.find_element_by_link_text(group_name.name).click()
+            self.app.driver.find_element_by_xpath('//*[@id="nb-1"]/body/div[2]/div[6]/div/div[3]/div[2]/div[3]/div/div[1]/div/div/div[4]/span').click()
             time.sleep(1)
+            # Ввести название новой группы
             self.app.driver.find_element_by_class_name('ui-dialog').find_element_by_class_name(
-                'mail-AbookPopup-Right').find_element_by_class_name('nb-input').send_keys(group_name.name + "qwe")
+                'mail-AbookPopup-Right').find_element_by_class_name('nb-input').send_keys(group_name.name)
             time.sleep(1)
+
             # Сохранить название группы
             self.app.driver.find_element_by_class_name('ui-dialog').find_element_by_class_name(
                 'mail-AbookPopup-Selected-Buttons').find_element_by_css_selector('button.nb-button').click()
             time.sleep(1)
         else:
-            # Создать группу
+            #  'Создать группу'
             self.app.driver.find_element_by_class_name('mail-LabelList').find_element_by_class_name(
                 '_nb-button-content').click()
             time.sleep(1)
