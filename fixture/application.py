5b9bd4a398bc7bb9cef13f25381700d23ae6b09d
Dimov <poterok@gmil.com>
2020-02-13 16:01:00 +0300

    Победил удаление группы

--- fixture/application.py
+++ fixture/application.py
@@ -10,7 +10,7 @@ class Application:
 
     def __init__(self):
         self.driver = webdriver.Chrome()
-        self.driver.implicitly_wait(60)
+        self.driver.implicitly_wait(5)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
 
--- fixture/group.py
+++ fixture/group.py
@@ -15,7 +15,8 @@ class GroupHelper:
 
     def create_new_group(self, group_name):
         # Создать группу
-        self.app.driver.find_element_by_class_name('mail-LabelList').find_element_by_class_name('_nb-button-content').click()
+        # self.app.driver.find_element_by_class_name('mail-LabelList').find_element_by_class_name('_nb-button-content').click()
+        self.app.driver.find_element_by_css_selector('div.mail-Layout-Aside-Inner').find_element_by_css_selector('div.ns-view-abook-left-box').find_element_by_css_selector('div.ns-view-abook-left').find_element_by_css_selector('span._nb-button-text').click()
         time.sleep(1)
 
         # Ввести название новой группы
@@ -51,40 +52,14 @@ class GroupHelper:
         # Если уже есть группа и нужно создать еще
 
     def if_has_other_group(self, group_name):
-
-        # Кнопка "Создать группу "
         try:
-            # a = self.app.driver.find_element_by_xpath('//*[@id="nb-1"]/body/div[2]/div[6]/div/div[3]/div[2]/div[3]/div/div[1]/div/div/div[3]/div/div[2]/button/span/span')
-            s = WebDriverWait(self.app.driver, 1).until(EC.presence_of_element_located((By.XPATH, '//*[@id="nb-1"]/body/div[2]/div[6]/div/div[3]/div[2]/div[3]/div/div[1]/div/div/div[3]/div/div[2]/button/span/span')))
+            self.app.driver.find_element_by_css_selector('div.mail-Layout-Aside-Inner').find_element_by_css_selector('div.ns-view-abook-left-box').find_element_by_css_selector('div.ns-view-abook-left').find_element_by_css_selector('span._nb-button-text')
+            print('Кнопка "Создать группу" есть')
+            self.create_new_group(group_name)
         except:
-            s = None
-        print(s)
-        if s is None:
-            # self.app.driver.find_element_by_link_text(group_name.name).click()
-            self.app.driver.find_element_by_xpath('//*[@id="nb-1"]/body/div[2]/div[6]/div/div[3]/div[2]/div[3]/div/div[1]/div/div/div[4]/span').click()
+            self.app.driver.find_element_by_css_selector('div.mail-Layout-Aside-Inner').find_element_by_css_selector('div.ns-view-abook-left-box').find_element_by_css_selector('div.mail-LabelList-Setup').find_element_by_css_selector('a.mail-LabelList-Setup_link').click()
             time.sleep(1)
-            # Ввести название новой группы
-            self.app.driver.find_element_by_class_name('ui-dialog').find_element_by_class_name(
-                'mail-AbookPopup-Right').find_element_by_class_name('nb-input').send_keys(group_name.name)
-            time.sleep(1)
-
-            # Сохранить название группы
-            self.app.driver.find_element_by_class_name('ui-dialog').find_element_by_class_name(
-                'mail-AbookPopup-Selected-Buttons').find_element_by_css_selector('button.nb-button').click()
-            time.sleep(1)
-        else:
-            #  'Создать группу'
-            self.app.driver.find_element_by_class_name('mail-LabelList').find_element_by_class_name(
-                '_nb-button-content').click()
-            time.sleep(1)
-
-            # Ввести название новой группы
-            self.app.driver.find_element_by_class_name('ui-dialog').find_element_by_class_name(
-                'mail-AbookPopup-Right').find_element_by_class_name('nb-input').send_keys(group_name.name)
-            time.sleep(1)
-
-            # Сохранить название группы
-            self.app.driver.find_element_by_class_name('ui-dialog').find_element_by_class_name(
-                'mail-AbookPopup-Selected-Buttons').find_element_by_css_selector('button.nb-button').click()
-            time.sleep(1)
-
+            self.app.driver.find_element_by_css_selector('div.setup-abook-groups').find_element_by_css_selector('ul.setup-abook-groups__inner').find_element_by_css_selector('li.setup-abook-group_editable').click()
+            # Нажать кнопку Удалить (в списке 3)
+            self.app.driver.find_element_by_xpath("//div[@class='setup-abook-groups-actions']/button[3]").click()
+            print('Удалил нахер')
